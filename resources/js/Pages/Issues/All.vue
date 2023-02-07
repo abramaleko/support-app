4dc4fd18d2f539813a2150091b6bc0259af55153
Abraham Maleko <abrahammaleko@gmail.com>
2023-02-07 12:14:24 +0300

    added hover effect to closing modal

--- resources/js/Pages/Issues/All.vue
+++ resources/js/Pages/Issues/All.vue
@@ -157,7 +157,7 @@ function closeModal(){
                     <div class="flex items-center justify-between pb-3">
                         <p class="text-2xl font-bold text-gray-900 dark:text-gray-50">Add New Issue</p>
                         <div class="z-50 cursor-pointer modal-close" @click="closeModal">
-                            <svg class="text-gray-500 fill-current" xmlns="http://www.w3.org/2000/svg" width="18"
+                            <svg class="text-gray-500 fill-current hover:text-white" xmlns="http://www.w3.org/2000/svg" width="18"
                                 height="18" viewBox="0 0 18 18">
                                 <path
                                     d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
