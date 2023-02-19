3a22f9cdb643c5606534dadd1040fe377ebf3530
Abraham Maleko <abrahammaleko@gmail.com>
2023-02-19 23:05:35 +0300

    added chat migration and model

---
+++ database/migrations/2023_02_11_075552_create_issue_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('issue_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('issue_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->longText('text');
+            $table->boolean('read')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('issue_histories');
+    }
+};
